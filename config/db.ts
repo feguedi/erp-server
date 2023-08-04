b5939d20977fc3c4643c3dfa7f33ceb0760b2f51
Ixe Fernández (Vagrant Debian) <feguedi@yahoo.com.mx>
2023-08-04 03:31:10 -0600

    Creado enum para Sequelize de config

--- config/db.ts
+++ config/db.ts
@@ -1,4 +1,6 @@
-const { Sequelize } = require('sequelize');
+import path from 'path';
+import type { Sequelize as SequelizeType } from 'sequelize'
+import { Sequelize } from 'sequelize';
 
 /* https://stackoverflow.com/questions/51528780/typescript-check-typeof-against-custom-type */
 const dbTypes = ['mysql', 'postgres', 'sqlite', 'mariadb', 'mssql', 'db2', 'snowflake', 'oracle'] as const;
@@ -8,6 +10,10 @@ const isDbType = (x: any): x is DbTypes => dbTypes.includes(x);
 const dbName = process.env.DB_NAME || 'erp-default';
 const dbUsername = process.env.DB_USERNAME || 'username';
 const dbPassword = process.env.DB_PASSWORD || 'password';
+const dbPort: number = Number.isNaN(process.env.DB_PORT) || !!process.env.DB_PORT
+  ? 5432
+  : Number.parseInt(process.env.DB_PORT);
+const dbStorage = path.resolve(process.env.DB_STORAGE);
  /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 const dialect: DbTypes = isDbType(process.env.DB_TYPE)
   ? process.env.DB_TYPE
@@ -22,14 +28,25 @@ const dialect: DbTypes = isDbType(process.env.DB_TYPE)
 //   dialect: 'sqlite',
 //   storage: 'path/to/database.sqlite'
 // });
-
-// Option 3: Passing parameters separately (other dialects)
-exports.sequelize = new Sequelize(
-  dbname,
+const sequelizeOptsGral = new Sequelize(
+  dbName,
   dbUsername,
   dbPassword,
   {
     host: process.env.DB_HOST || 'http://localhost',
+    port: dbPort,
     dialect,
   },
 );
+
+// Option 3: Passing parameters separately (other dialects)
+export const sequelize: SequelizeType = {
+  'sqlite': new Sequelize('sqlite::memory:'),
+  'mysql': sequelizeOptsGral,
+  'postgres': sequelizeOptsGral,
+  'mariadb': sequelizeOptsGral,
+  'mssql': sequelizeOptsGral,
+  'db2': sequelizeOptsGral,
+  'snowflake': sequelizeOptsGral,
+  'oracle': sequelizeOptsGral,
+}[dialect];
